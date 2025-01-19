7c00bf09c4711db5d5471709b32ebdab66d837c3
Matthias Reichl <hias@horus.com>
2025-01-19 23:03:50 +0100

    linux (RPi): update to 6.6.72-545cb71
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default"
     ;;
   raspberrypi)
-    PKG_VERSION="dfff38316c1284c30c68d02cc424bad0562cf253" # 6.6.70
-    PKG_SHA256="daf3f79ac5da3b8a3257ef1f8dec0234f713ef66f8f2365bd4fb83e6e503d356"
+    PKG_VERSION="545cb717f0ad24d3353ad6c1c19741970e8d9d50" # 6.6.72
+    PKG_SHA256="1a1cb9841f9169587641a1cc520903ca89f4f238b76e308461b98a13418e4041"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
